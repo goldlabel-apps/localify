b0d99d16a608591b57254ae5ea88809efa193ad1
listingslab <listingslab@gmail.com>
2021-06-07 20:35:29 +1000

    Better MenuDial

--- src/components/MenuDial.jsx
+++ src/components/MenuDial.jsx
@@ -10,6 +10,9 @@ import {
     SpeedDialAction,
 } from '@material-ui/lab/'
 import { Icon } from '../theme'
+import { 
+  navigateTo,
+} from '../redux/app/actions'
 
 const useStyles = makeStyles((theme) => ({
   speedDial: {
@@ -24,7 +27,12 @@ const useStyles = makeStyles((theme) => ({
 }))
 
 const actions = [
-  { icon: <Icon icon={ `github` } color={ `black` } />, name: 'GitHub' },
+  { 
+    icon: <Icon icon={ `github` } color={ `white` } />,
+    url: `https://github.com/orgs/listingslab-software/projects/14?fullscreen=true`,
+    target: `_blank`,
+    name: 'GitHub',
+  },
 ]
 
 export default function MenuDial() {
@@ -46,17 +54,22 @@ export default function MenuDial() {
             ariaLabel={ `Dial Menu` }
             direction={ `down` }
             className={ clsx( classes.speedDial) }
-            icon={ <SpeedDialIcon className={ classes.speedDialIcon } /> }
+            icon={ <SpeedDialIcon 
+                    icon={<Icon icon={`menu`} />} 
+                    className={ classes.speedDialIcon } 
+                  /> }
             onClose={ handleClose }
             onOpen={ handleOpen }
             open={ open }>
-
             {actions.map((action) => (
               <SpeedDialAction
                 key={ action.name }
                 icon={ action.icon }
                 tooltipTitle={ action.name }
-                onClick={ handleClose }
+                onClick={ () => {
+                  navigateTo( action.url, action.target )
+                  handleClose()
+                } }
               />
             ))}
           </SpeedDial>
